3354e7f0822691eee101cf5286eb0ddc554957ee
matthewaramis <matthew.aramis@gmail.com>
2018-11-09 13:31:50 -0800

    Init commit.

---
+++ themes/project-04-theme/js/custom.js
@@ -0,0 +1,11 @@
+(function($) {
+  $(document).ready(function() {
+    //code goes here
+    $('body').append('hello');
+
+    //TO DO: Add search form toggle (focus and blur are key)
+  });
+})(jQuery);
+
+//IIFE
+//Immediately Invoked Function Expression
