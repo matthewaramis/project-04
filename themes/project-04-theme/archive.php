97ab82e222b27c1d887fb4dbc4339b579340367a
matthewaramis <matthew.aramis@gmail.com>
2018-11-27 21:11:23 -0800

    Style archive for journal posts.

--- themes/project-04-theme/archive.php
+++ themes/project-04-theme/archive.php
@@ -7,6 +7,7 @@
 
 get_header(); ?>
 
+<div class="archive-content">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
@@ -38,6 +39,7 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
+</div>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
